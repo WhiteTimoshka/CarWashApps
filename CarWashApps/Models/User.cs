1b5de420824490f28bc5daa6f178f9827948fc24
Егор Ведров <whitetimoshka@hotmail.com>
2023-11-27 15:40:39 +0500

    Создание контекста данных, написание строки подключения к любому экземпляру MS SQL Server, связь контекста и строки

---
+++ CarWashApps/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarWashApps.Models
+{
+    public class User : IdentityUser
+    {
+        //Дополнительные поля для каждого пользователя
+        public int Id { get; set; }
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Введите Имя")]
+        public string NameUser { get; set; }
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Введите Фамилию")]
+        public string Surname { get; set; }
+        public DateTime RegDate { get; set; }
+        public string IdRight { get; set; }
+
+        //Навигационные св-ва
+    }
+}
